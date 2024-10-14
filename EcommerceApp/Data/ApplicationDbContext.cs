b8a651a591a819358133351a5481f534b3b4827b
Joel Baena <joeebaena@gmail.com>
2024-10-14 15:28:42 -0500

    Commit primario
    se configuró la conexión a la base de datos, hay una primera clase para los datos

---
+++ EcommerceApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using EcommerceApp.Models; // Asegúrate de tener este namespace para el modelo Producto
+
+
+namespace EcommerceApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Agrega el DbSet para los productos
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
